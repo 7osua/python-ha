abaad80b9c691b501a7a3825aaf910b1621e416f
7osua <j7.manullang@gmail.com>
2024-07-02 18:14:53 +0700

    [^] Adding description about how the static method works. Know that the static methods cannot access or modify class state. Using the "@staticmethod" to define a method. Knowing that the static method can help us to create a utility class to group functions that have some logical relationships.

--- classes_and_objects/static_method.py
+++ classes_and_objects/static_method.py
@@ -2,9 +2,61 @@
 
 
 # Introduction to static methods
+"""
+Previously, you have learned about instance methods that are
+bound to a specific instance. It means that instance methods
+can access and modify the state of the bound object.
+
+Also, you learned about 'class methods' that are bound to a
+class. The class methods can access and modify the class state.
+
+Unlike instance methods, static methods aren't bound to an
+object. In other words, static methods cannot access and modify
+an object state.
+
+in addition, Python doesn't implicitly pass the 'cls' parameter
+(or the 'self') to static methods.
+Therefore, static methods cannot access and modify
+the class state.
+
+In practice, you use the static methods to define utility methods
+or group functions that have some logical relationships in a class.
+
+to define a statice method, we can use the '@staticmethod' decorator
+
+    class ClassName:
+
+        @staticmethod
+        def static_method_name(param_list):
+            pass
+
+to call a static method, we use this syntax.
+
+    ClassName.static_method_name()
+
+"""
+
+# Python static methods Vs. class methods
+"""
+Class Methods
+- Python implicitly pass the 'cls' argument to class methods.
+- Class methods can access and modify the class state.
+- Use '@classmethod' decorators to define class methods.
+
+Static Methods
+- Python doesn't implicitly pass the 'cls' argument
+  to static methods.
+- Static methods cannot access or modify the class state.
+- Use '@staticmethod' decorators to define static methods.
+"""
 
 
 # Static method examples
+
+
+# Define a class that has static methods for
+# converting temperatures between
+# Celcius, Fahrenheit and Kelvin
 
 
 class TemperatureConverter:
@@ -50,10 +102,19 @@ class TemperatureConverter:
         return f"{value} {symbol}"
 
 
+# call the 'TemperatureConverter' class
 f = TemperatureConverter.celcius_to_fahrenheit(35)
 print(TemperatureConverter.format(f, TemperatureConverter.FAHRENHEIT))
+
+# [Output] 95.0 °F
 
 """
 Learn about static methods and learn
 how to use static methods to create a utility class.
+
+[Summary]
+- Use static methods to define utility methods or group
+  a logically related functions into a class.
+- Use the '@staticmethod' decorator to define a static
+  method.
 """
